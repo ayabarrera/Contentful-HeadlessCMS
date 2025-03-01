55022bc36f3098a320954530fd8bd3c71921782c
ayabarrera <alyannabarrera@gmail.com>
2025-03-01 10:53:23 -0700

    added the return -> with headerfooter (JSX) and render the name, image, brand, model, and kind to our main webpage

--- app/page.jsx
+++ app/page.jsx
@@ -33,6 +33,27 @@ export default function Home() {
   }, []);
 
   return (
- 
+    <HeaderFooter>
+      <div>
+        <h1 className={styles.h1Header}>Aya's Collection</h1>
+        <div className={styles.gridContainer}>
+          {trinkets.map((trinket) => ( 
+            <div key={trinket.sys.id} className={styles.itemContainer}>
+              <h2 className={styles.itemName}>"{trinket.fields.name}"</h2>
+              {trinket.fields.image && (
+                <img
+                  src={trinket.fields.image.fields.file.url}
+                  alt={trinket.fields.name}
+                  className={styles.itemImage}
+                />
+              )}
+              {trinket.fields.brand && <p><i>Brand: </i> {trinket.fields.brand}</p>}
+              {trinket.fields.model && <p><i>Model: </i> {trinket.fields.model}</p>}
+              {trinket.fields.kind && <p><i>Kind: </i> {trinket.fields.kind}</p>}
+            </div>
+          ))}
+        </div>
+      </div>
+    </HeaderFooter>
   );
 }
