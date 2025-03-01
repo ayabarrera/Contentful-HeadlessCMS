dc0e53173e8e7c5a2460bb4c6d8e70873725599b
ayabarrera <alyannabarrera@gmail.com>
2025-03-01 10:52:38 -0700

    added the async function for getting the information from APi then convert it to JSON

--- app/api/contentful/route.jsx
+++ app/api/contentful/route.jsx
@@ -4,3 +4,23 @@ const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
+
+export async function GET() {
+  try {
+    const response = await client.getEntries({ content_type: 'trinkets' });
+    return new Response(JSON.stringify(response.items), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return new Response(JSON.stringify({ error: 'Failed to fetch data' }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
